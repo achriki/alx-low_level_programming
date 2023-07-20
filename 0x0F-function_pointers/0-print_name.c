cdb1a2af3dc008d225147d104cc38a09bbdbbbf0
achriki <adam.chkpr@gmail.com>
2023-07-19 17:49:30 -0700

    fix issue in task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "function_pointers.h"
-
 /**
  * print_name - print name
  * @name: name
  * @f: func pointer to  printer
  **/
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (f)
