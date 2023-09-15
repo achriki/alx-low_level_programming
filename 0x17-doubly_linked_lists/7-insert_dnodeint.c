628554196d9ac74373bdf68da8598ab4920e0500
achriki <adam.chkpr@gmail.com>
2023-09-14 17:03:52 -0700

    rebuild task 7 again

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,50 +1,58 @@
 #include "lists.h"
+
 /**
- * insert_dnodeint_at_index - insert at index
- * @h: head of list
- * @idx: index of node
- * @n: data
- * Return: pointer on node
+ * insert_dnodeint_at_index - function that inserts a new node at,
+ * a given position.
+ * @h: pointer to pointer to the h of linked list.
+ * @idx: index of the list where the new  node should be added.
+ * @n: value of the new node.
+ *
+ * if it is not possible to add the new node at index idx, do not,
+ * add the new node and return NULL.
+ *
+ * Return:  the address of the new node, or NULL if it failed.
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current;
-	dlistint_t *new;
-	unsigned int pos = 0;
+	dlistint_t *new_node;
+	dlistint_t *head;
+	unsigned int i;
 
-	if (h == NULL)
-		return (NULL);
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	if (idx == 0)
-	{
-		new->next = *h;
-		new->prev = NULL;
-		if (*h != NULL)
-			(*h)->prev = new;
-		*h = new;
-		return (new);
-	}
+	new_node = NULL;
+	if (idx == 0)	/* insert node at beginning of list */
+		new_node = add_dnodeint(h, n);
 	else
 	{
-		current = *h;
-		while (current)
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
 		{
-			if (idx == pos)
+			if (i == idx)
 			{
-				new->next = current;
-				new->prev = current->prev;
-				if (current->prev != NULL)
-					current->prev->next = new;
-				current->prev = new;
-				return (new);
+				/* insert note at the end of list */
+				if (head->next == NULL)
+					new_node = add_dnodeint_end(h, n);
+				else
+				{
+					new_node = malloc(sizeof(dlistint_t));
+					if (new_node != NULL)
+					{
+						new_node->n = n;
+						new_node->next = head->next;
+						new_node->prev = head;
+						head->next->prev = new_node;
+						head->next = new_node;
+					}
+				}
+				break;
 			}
-			current = current->next;
-			pos++;
+			head = head->next;
+			i++;
 		}
 	}
-	free(new);
-	return (NULL);
+	return (new_node);
 }
